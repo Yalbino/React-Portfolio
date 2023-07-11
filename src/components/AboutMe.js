e9ed12d55c0f7796f036a5d967c8ffac50cbd52b
Yalbino <93532102+Yalbino@users.noreply.github.com>
2023-07-10 23:02:09 -0400

    Add files via upload

---
+++ src/components/AboutMe.js
@@ -0,0 +1,14 @@
+function AboutMe() {
+    return (
+      <section>
+        <h2>About Me</h2>
+        <p>
+          Hi, I'm Sam Student, an aspiring full-stack developer. I have a passion for web development and enjoy creating dynamic and interactive websites. I have experience with HTML, CSS, JavaScript, and React. I am constantly learning and exploring new technologies to enhance my skills as a web developer.
+        </p>
+        <img src={ME.png} alt="Sam Student" />
+      </section>
+    );
+  }
+  
+  export default AboutMe;
+  
---
+++ src/components/Header.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+
+function Header() {
+  return (
+    <header className="header">
+      <div className="header-content">
+        <h1 className="name">HELLO I'M Yaraili Albino </h1>
+        <p className="title">Freelance Web Designer based in North Carolina, USA.</p>
+        
+      </div>
+    </header>
+  );
+}
+
+export default Header;
+
+
